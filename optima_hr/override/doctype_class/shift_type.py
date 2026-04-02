384f8350b87c4781b8f01b3d9817a9355e9b4623
FawazAlhafiz <fawaztwix111@gmail.com>
2026-04-02 13:49:11 +0300

    refactor: remove debug print statements in OptimaShiftType

--- optima_hr/override/doctype_class/shift_type.py
+++ optima_hr/override/doctype_class/shift_type.py
@@ -21,12 +21,10 @@ class OptimaShiftType(ShiftType) :
             or not self.process_attendance_after
             or not self.last_sync_of_checkin
         ):
-            print("Heeloo" * 50)
             return
-        print("shift")
+        
         skip_employee = get_skip_employees()
         logs = self.get_employee_checkins(skip_employee)
-        print(logs)
         for key, group in groupby(logs, key=lambda x: (x["employee"], x["shift_start"])):
             employee = key[0]
             attendance_date = key[1].date()
@@ -59,7 +57,6 @@ class OptimaShiftType(ShiftType) :
         frappe.db.commit() 
         
         assigned_employees = self.get_assigned_employees(self.process_attendance_after, True)
-        print(assigned_employees)
 
         for batch in create_batch(assigned_employees, EMPLOYEE_CHUNK_SIZE):
             for employee in batch :
