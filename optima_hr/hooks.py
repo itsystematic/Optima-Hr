a7c8818ad5ed45ae8dcfcd5bb0ec354694481c9f
FawazAlhafiz <fawaztwix111@gmail.com>
2026-04-09 12:33:59 +0300

    fix: update scheduler_events to use a list for create_employee_checkin timing

--- optima_hr/hooks.py
+++ optima_hr/hooks.py
@@ -170,7 +170,9 @@ doc_events = {
 scheduler_events = {
     "cron": {
         # "0 0 1 * *": "optima_hr.tasks.cron.make_attendance_absent_for_unmarked_employee",
-        "0 0,8,16 * * *": "optima_hr.fingerprint.utils.create_employee_checkin",
+        "*/15 * * * *": [
+            "optima_hr.fingerprint.utils.create_employee_checkin",
+        ],
     },
 # 	"all": [
 # 		"optima_hr.tasks.all"
